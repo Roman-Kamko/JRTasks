cf0a8dec169140eb984e9fc2d77a669ea4e6bf67
RomanKamko <r.kamko@mail.ru>
2023-02-13 14:59:07 +0400

    Arrays. Task 1

---
+++ src/syntax/arrays/ArraysTask1.java
@@ -0,0 +1,12 @@
+package syntax.arrays;
+
+public class ArraysTask1 {
+    public static int[] desert;
+    public static void main(String[] args) {
+        /*В методе main проинициализируй переменную desert массивом элементов типа int и размером 10. Третьему элементу
+        массива присвой значение 5. Остальные элементы не трогай.*/
+
+        desert = new int[10];
+        desert[2] = 5;
+    }
+}
