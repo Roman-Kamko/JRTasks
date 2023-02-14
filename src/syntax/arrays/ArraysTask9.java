ac3eddcd3bd1be26afdb40b8a42b7974ad0bac6d
RomanKamko <r.kamko@mail.ru>
2023-02-14 16:20:02 +0400

    Arrays. Task 9

--- src/syntax/arrays/ArraysTask9.java
+++ src/syntax/arrays/ArraysTask9.java
@@ -2,23 +2,24 @@ package syntax.arrays;
 
 import java.util.Scanner;
 
-public class ArraysTask8 {
+public class ArraysTask9 {
     /*Ввести с клавиатуры число N.
-    Считать N целых чисел и заполнить ими массив.
-    Найти минимальное число среди элементов массива и вывести в консоль.*/
+    Считать N целых чисел и заполнить ими массив array.
+    Найти максимальное число среди элементов массива.*/
+
     public static int[] array;
 
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         int countNum = sc.nextInt();
         array = new int[countNum];
-        int minNum = Integer.MAX_VALUE;
+        int maxNum = Integer.MIN_VALUE;
         for (int i = 0; i < array.length; i++) {
             array[i] = sc.nextInt();
-            if (minNum > array[i]) {
-                minNum = array[i];
+            if (maxNum < array[i]) {
+                maxNum = array[i];
             }
         }
-        System.out.println(minNum);
+        System.out.println(maxNum);
     }
 }
